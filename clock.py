5e4abcca78881f2d497b96eea9dafc7c97905edc
NAME <NO_E-MAIL>
2018-03-21 00:50:30 +0900

    any

--- clock.py
+++ clock.py
@@ -1,28 +1,44 @@
 #coding: utf-8
 #Clock 0.0.0
 #Copyright 2017 Cabbage All Rights Reserved.
+#python C:\Users\owner\code\project01_Clock\clock.py
 
 from tkinter import *
 import time, sys
 
 ver = 'Clock 0.0.0\nCopyright 2017 Cabbage All Rights Reserved.'
-help = '''
-<<HELP>>
--c [color]   : Set color of text [color].
--b [color]   : Set color of background [color].
--h or --help : Show this help and exit.
--v           : Show version and exit.'''
+help = '''<<HELP>>
+-c [color]            : Set color of text [color].
+-b [color]            : Set color of background [color].
+-v                    : Show version and exit.
+-f [FONT]             : Set font of text [FONT]
+-h or --help          : Show this help and exit.
+-H or --japanese-help : Show Japanese help and exit.'''
+jhelp = '''<<ヘルプ>>
+-c [color]                  : 文字色を[color]にします。
+-b [color]                  : 背景色を[color]にします。
+-v                          : バージョンを表示し、終了します。
+-f [FONT]                   : フォントを[FONT]にします。
+-h もしくは --help          : 英語のヘルプを表示し、終了します。
+-H もしくは --japanese-help : このヘルプを表示し、終了します。'''
 print(ver)
 if '-h' in sys.argv or '--help' in sys.argv:
     print(help)
+    exit()
+elif '-H' in sys.argv or '--japanese-help' in sys.argv:
+    print(jhelp)
     exit()
 
 if '-v' in sys.argv:
     exit()
 
+textsize = 50
+if '-s' in sys.argv:
+    textsize = int(sys.argv[sys.argv.index('-s') + 1])
+
 tk = Tk()
 tk.title('Clock')
-ca = Canvas(tk, width=200, height=50)
+ca = Canvas(tk, width=450, height=65)
 ca.pack()
 tk.update()
 
@@ -49,9 +65,14 @@ def start():
     global textcolor
     global backgroundcolor
     global printdebug
+    global textsize
     readargs()
     del_id = [ca.create_rectangle(0, 0, 500, 500, fill=backgroundcolor)]
-    id = ca.create_text(100, 30, text='', fill=textcolor, font=('Source Code Pro Medium', 30))
+    textsize = 70
+    fontname = 'Source Code Pro Medium'
+    if '-f' in sys.argv:
+        fontname = sys.argv[sys.argv.index('-f') + 1]
+    id = ca.create_text(220, 30, text='', fill=textcolor, font=(fontname, textsize))
 
 def rv():
     global id
--- compile.py
+++ compile.py
@@ -1,2 +1,2 @@
-#C:\Users\owner\code\project01_Clock\compile.py
+#C:\Users\owner\code\project01_Clock\__pycache__\clock.cpython-36.pyc
 import clock
